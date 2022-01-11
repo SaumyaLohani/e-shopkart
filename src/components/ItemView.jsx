1b9f16d8fb869939233836590a2a247de2bcdfc8
PRT28 <56737649+PRT28@users.noreply.github.com>
2022-01-11 13:00:43 +0530

    next commit

--- src/components/ItemView.jsx
+++ src/components/ItemView.jsx
@@ -2,10 +2,13 @@ import React,{useState,useEffect} from 'react';
 import axios from "axios";
 import {useParams} from "react-router-dom";
 import {Row,Col,Button, Modal} from "react-bootstrap";
-import {supabase, analytics} from "../supabase";
+import {supabase, analytics, remoteConfig} from "../supabase";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus, faCartPlus} from '@fortawesome/free-solid-svg-icons';
 import {logEvent} from "firebase/analytics";
+import { getNumber } from "firebase/remote-config";
+import { fetchConfig, activate } from "firebase/remote-config";
+import Loader from "react-loader-spinner";
 
 function ItemView(props){
 
@@ -13,22 +16,32 @@ function ItemView(props){
     const [quant,setQuant]=useState(1);
     let {id} =useParams();
     const [show, setShow] = useState(false);
-    const [message,setMessage] = useState("")
+    const [message,setMessage] = useState("");
+    const [load,setLoad]=useState(true);
+    const [dis,setDis] = useState(0)
     const handleClose = () => {
         setShow(false);
         if(props.uid===undefined){
             window.location.href="/login";
         }
     }
+    
 
     const handleShow = () => setShow(true);
 
     useEffect(()=> { 
         const getData= async() =>{
-            console.log(props)
             try{
+                setLoad(true);
+                await fetchConfig(remoteConfig).then((d)=>{
+                    activate(remoteConfig)
+                })
+                const val = getNumber(remoteConfig, "discount");
+                console.log(val);
+                setDis(val);
                 const res = await axios.get("https://fakestoreapi.com/products/"+id);
                 setData(res.data);
+                setLoad(false);
             } catch(e){
                 console.log(e)
             }
@@ -64,34 +77,54 @@ function ItemView(props){
             handleShow();
         }
     }
-    
-    return(
-        <>
-        {props&& <div className="item">
-            <Row>
-                <Col>
-                    <img src={data.image} alt="" />
-                </Col>
-                <Col>
-                    <h1>{data.title}</h1><br />
-                    <p>{data.description}</p>
-                    <p>Price: {data.price} $</p>
-                    <p>Quantity:&emsp; <FontAwesomeIcon icon={faMinus} onClick={()=>minus()} /> &nbsp; {quant} &nbsp; <FontAwesomeIcon icon={faPlus} onClick={()=>plus()} /></p><br/>
-                    <Button variant="outline-primary" onClick={addItem} ><FontAwesomeIcon icon={faCartPlus} />&ensp;Add to Cart</Button>
-                </Col>
-            </Row>
-        </div>}
-        <Modal show={show} onHide={handleClose}>
-        <Modal.Body>{message}</Modal.Body>
-        <Modal.Footer>
-          <Button variant="outline-secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-        </>
+    if(load===false){
+        return(
+            <>
+            {props&& <div className="item">
+                <Row>
+                    <Col>
+                        <img src={data.image} alt="" />
+                    </Col>
+                    <Col>
+                        <h1>{data.title}</h1><br />
+                        <p>{data.description}</p>
+                        {dis>0?<>
+                            <p className="strike">Price: {data.price}</p>
+                            <p>Price: {data.price-(data.price*(dis/100))} $</p>
+                        </>:
+                        <p>Price: {data.price}</p>
+                        }
+                        <p>Quantity:&emsp; <FontAwesomeIcon icon={faMinus} onClick={()=>minus()} /> &nbsp; {quant} &nbsp; <FontAwesomeIcon icon={faPlus} onClick={()=>plus()} /></p><br/>
+                        <Button variant="outline-primary" onClick={addItem} ><FontAwesomeIcon icon={faCartPlus} />&ensp;Add to Cart</Button>
+                    </Col>
+                </Row>
+            </div>}
+            <Modal show={show} onHide={handleClose}>
+            <Modal.Body>{message}</Modal.Body>
+            <Modal.Footer>
+              <Button variant="outline-secondary" onClick={handleClose}>
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+            </>
+            
+        );
+    }else{
+        return(
+            <div className="item">
+                <Loader
+                type="Bars"
+                color="#35589A"
+                height={100}
+                width={100}
+              />
+            </div>
+            
+        );
         
-    );
+    }
+    
 }
 
 export default ItemView;
--- src/supabase.js
+++ src/supabase.js
@@ -2,6 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
+import { getRemoteConfig } from "firebase/remote-config";
 
 const SUPABASE_URL = 'https://uexpkvyhppcpxuhgbjxw.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDY2NTExOCwiZXhwIjoxOTU2MjQxMTE4fQ.fgq8i0jvMRB0ppyPoLdIA8ga4YikAJoC1oV56ZY8ciE'
@@ -21,3 +22,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const analytics = getAnalytics(app);
+const remoteConfig = getRemoteConfig(app);
+remoteConfig.settings.minimumFetchIntervalMillis = 0;
+remoteConfig.defaultConfig = {
+  "discount": 50
+};
+export {remoteConfig};
